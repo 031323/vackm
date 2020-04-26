f54af6fd01f425117f019f0f1f44ea90ecbb5cc2
142434 <utkars.rajput@gmail.com>
2020-04-26 08:55:21 +0530

    3

--- vadanam.py
+++ vadanam.py
@@ -5,21 +5,15 @@ class vadanam:
 		s._n0bh0s=int(bh0s*n0aa/m0aa)
 		s._m0aa=m0aa
 		s._n0aa=n0aa
-		s._m0d=numpy.zeros(bh0s,dtype=numpy.float)
-		s._m0v=numpy.zeros(bh0s,dtype=numpy.float)
-		s._n0d=numpy.zeros(s._n0bh0s,dtype=numpy.float)
-		s._n0v=numpy.zeros(s._n0bh0s,dtype=numpy.float)
+		s._m0d=[0.]*bh0s
+		s._m0v=[0.]*bh0s
+		s._n0d=[0.]*s._n0bh0s
+		s._n0v=[0.]*s._n0bh0s
 		s._n0s=bh0s-s._n0bh0s
 		if s._n0s==0:s._n0s=1
-		s.m0vi=numpy.full(bh0s,1,dtype=numpy.float)
-		s.n0vi=numpy.full(s._n0bh0s,1,dtype=numpy.float)
+		s.m0vi=[1.]*bh0s
+		s.n0vi=[1.]*s._n0bh0s
 		s.n0vi[0]=0
-		
-		s._0m0d=numpy.zeros(s._bh0s+1,dtype=numpy.float)
-		s._0m0v=numpy.zeros(s._bh0s+1,dtype=numpy.float)
-		s._0n0d=numpy.zeros(s._n0bh0s+1,dtype=numpy.float)
-		s._0n0v=numpy.zeros(s._n0bh0s+1,dtype=numpy.float)
-		
 		
 	def yojanam(s,sth,m):
 		s._m0v[int(s._bh0s*sth)]+=m/2
@@ -27,8 +21,10 @@ class vadanam:
 	def nirgatih(s):
 		return s._m0v[s._bh0s-1]+s._m0d[s._bh0s-1]+s._n0v[s._n0bh0s-1]+s._n0d[s._n0bh0s-1]
 	def pravaahah(s):
-		
-		m0d,m0v,n0d,n0v=s._0m0d,s._0m0v,s._0n0d,s._0n0v
+		m0d=[0.]*(s._bh0s+1)
+		m0v=[0.]*(s._bh0s+1)
+		n0d=[0.]*(s._n0bh0s+1)
+		n0v=[0.]*(s._n0bh0s+1)
 		
 		for bh in range(1,s._bh0s):
 			a=(s.m0vi[bh-1]-s.m0vi[bh])/(s.m0vi[bh-1]+s.m0vi[bh])*(s._m0d[bh-1]+s._m0v[bh])
@@ -52,11 +48,12 @@ class vadanam:
 		a=(2*s.n0vi[0]-y)/y
 		n0d[0]=a*s._n0v[0]+(1+a)*(s._m0d[s._n0s-1]+s._m0v[s._n0s])
 		
-		s._m0d[0:s._bh0s]=m0d[0:s._bh0s]
-		s._m0v[0:s._bh0s]=m0v[1:1+s._bh0s]
-		
-		s._n0d[0:s._n0bh0s]=n0d[0:s._n0bh0s]
-		s._n0v[0:s._n0bh0s]=n0v[1:1+s._n0bh0s]
+		for bh in range(0,s._bh0s):
+			s._m0d[bh]=m0d[bh]
+			s._m0v[bh]=m0v[bh+1]
+		for bh in range(0,s._n0bh0s):
+			s._n0d[bh]=n0d[bh]
+			s._n0v[bh]=n0v[bh+1]
 		
 		return s._m0aa/s._bh0s/s0g
 
